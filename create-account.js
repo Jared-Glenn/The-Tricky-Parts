bc8553ebb2eaefff65163ee64cfa545a1ad3bc0d
Jared <jared_glenn@yahoo.com>
2024-05-25 13:36:21 -0600

    finish createAccount (closure)

--- create-account.js
+++ create-account.js
@@ -1,5 +1,36 @@
-function createAccount(pin, amount) {
-
+function createAccount(pin, amount=0) {
+    return {
+        checkBalance(pinCheck) {
+            if (pinCheck !== pin) {
+                return "Invalid PIN."
+            }
+            return `$${amount}`;
+        },
+        deposit(pinCheck, added) {
+            if (pinCheck !== pin) {
+                return "Invalid PIN."
+            }
+            amount += added;
+            return `Successfully deposited $${added}. Current balance: $${amount}.`
+        },
+        withdraw(pinCheck, spent) {
+            if (pinCheck !== pin) {
+                return "Invalid PIN."
+            }
+            if (spent > amount) {
+                return "Withdrawal amount exceeds account balance. Transaction cancelled."
+            }
+            amount -= spent;
+            return `Successfully withdrew $${spent}. Current balance: $${amount}.`;
+        },
+        changePin(pinCheck, newPin) {
+            if (pinCheck !== pin) {
+                return "Invalid PIN."
+            }
+            pin = newPin;
+            return "PIN successfully changed!"
+        }
+    }
 }
 
 module.exports = { createAccount };
