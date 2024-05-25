5f327e75a43dac76a8907b412dcb74bef1707c94
Jared <jared_glenn@yahoo.com>
2024-05-25 15:13:41 -0600

    finish curriedAdd (closure)

--- curried-add.js
+++ curried-add.js
@@ -1,5 +1,14 @@
-function curriedAdd(total) {
-
+function curriedAdd(total=null) {
+    if (total === null) {
+        return 0;
+    }
+    return function adder(added=null) {
+        if (added === null) {
+            return total;
+        }
+        total += added
+        return adder;
+    }
 }
 
 module.exports = { curriedAdd };
