99e19d1d05759460888e63fbd071f29824f71912
Jared <jared_glenn@yahoo.com>
2024-05-25 13:00:45 -0600

    finish guessing game (closure)

--- guessing-game.js
+++ guessing-game.js
@@ -1,5 +1,25 @@
 function guessingGame() {
-
+    const answer = Math.ceil(Math.random() * 99);
+    let guesses = 0;
+    let gameOver = false;
+    return function game(guess) {
+        if (!gameOver) {
+            guesses++;
+            if (guess == answer) {
+                gameOver = true;
+                return `You win! You found ${answer} in ${guesses} guesses.`;
+            }
+            else if (guess < answer) {
+                return `${guess} is too low!`;
+            }
+            else {
+                return `${guess} is too high!`;
+            }
+        }
+        else {
+            return "The game is over, you already won!";
+        }
+    }
 }
 
 module.exports = { guessingGame };
